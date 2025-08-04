c428f98214c629f47265c8861455aa5aa0bf4fda
hannu <hannu.eloranta@gmail.com>
2025-08-04 19:46:17 +0300

    add open dbase

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QtSql>
+
+extern bool openDatabase(const QString& name)
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    db.setDatabaseName(name);
+    return db.open();
+}
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    if (!openDatabase("Alarm.sqlite"))
+    {
+        qDebug() << "Cannot open database";
+        return -1;
+    }
+    MainWindow window;
+    window.show();
+    return app.exec();
 }
